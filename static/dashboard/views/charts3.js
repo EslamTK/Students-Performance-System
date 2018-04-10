47914deac6acbead0347ffceca2e208a6b17118d
Mohamed Mostafa <mohamed.feeshar@gmail.com>
2018-04-10 18:05:52 +0200

    charts can take dynamic data -- labels takes dynamic data

--- static/dashboard/views/charts3.js
+++ static/dashboard/views/charts3.js
@@ -2,21 +2,48 @@ $(function () {
     'use strict';
 
     var randomScalingFactor = function () {
-        return Math.round(Math.random() * 100)
+        return Math.round(Math.random() * 10)
     };
+    
+    var my_data = document.getElementById("myVar").value;
+    
+    //formating data to valid json format
+    var data = my_data.slice(10,my_data.length,my_data)
+    data = data.replace('>','');
+    data = data.replace(/'/g,'"');
+    data = JSON.parse(data);
+
+    //collecting data
+    var label_data = [];
+    var midpass = [];
+    var finalpass = [];
+    for(var i = 0; i < data.length; i++){
+        
+        if(data[i].midterm_pass == 0){
+            //for data to be visual remove if valid data entered
+            data[i].midterm_pass = randomScalingFactor();
+            //for data to be visual remove if valid data entered
+            data[i].final_pass = randomScalingFactor();
+
+            label_data.push(data[i].course__name);
+            midpass.push(data[i].midterm_pass);
+            finalpass.push(data[i].final_pass);
+        }     
+    }
+
+
     var lineChartData = {
-        labels: ['Math', 'Programming 1', 'Data Structure', 'Network', 'Security', 'Software Engineering', 'Computer Vision'],
+        labels: label_data ,
         datasets: [
             {
-                label: 'Pass',
+                label: 'Midterm Pass',
                 backgroundColor: 'rgba(220,220,220,0.2)',
                 borderColor: 'rgba(220,220,220,1)',
                 pointBackgroundColor: 'rgba(220,220,220,1)',
                 pointBorderColor: '#fff',
-                data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
+                data: midpass
             },
             {
-                label: 'Fail',
                 label: 'Fail',
                 backgroundColor: 'rgba(151,187,205,0.2)',
                 borderColor: 'rgba(151,187,205,1)',
@@ -28,13 +55,13 @@ $(function () {
             }
             ,
             {
-                label: 'Pass',
+                label: 'Final Pass',
                 backgroundColor: 'rgba(234,209,204,0.2)',
                 borderColor: 'rgba(234,209,204,1)',
                 pointBackgroundColor: 'rgba(234,209,204,1)',
 
                 pointBorderColor: '#fff',
-                data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
+                data: finalpass
             }
             ,
             {
