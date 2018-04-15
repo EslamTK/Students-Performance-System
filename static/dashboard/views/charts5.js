9f024de20d5c6f083ed4c91f004afdd755fb33b9
Mohamed Mostafa <mohamed.feeshar@gmail.com>
2018-04-15 22:16:11 +0200

    admistrator templates , educators(chart not compeleted)

--- static/dashboard/views/charts5.js
+++ static/dashboard/views/charts5.js
@@ -2,64 +2,56 @@ $(function () {
     'use strict';
 
     var randomScalingFactor = function () {
-        return Math.round(Math.random() * 100)
+        return Math.round(Math.random() * 255)
     };
+    //getting data from hidden input field
+    var my_data = document.getElementById("myData").value;
+    console.log(my_data);
+    
+    //formating data to valid json format
+    var data = my_data.slice(10,my_data.length,my_data)
+    data = data.replace('>','');
+    data = data.replace(/'/g,'"');
+    data = JSON.parse(data);
+    console.log(data);
+
+    var label_data = [];
+    var review_item = new Array(3);
+    var values = [];
+    for(var i = 0; i < data.length; i++){
+        /*if(label_data.indexOf(data[i].year)){
+            if(review_item.indexOf(data[i].review_item__name)){
+                values[review_item.indexOf(data[i].review_item__name)].push(data[i].rate__avg);
+                console.log(data[i].rate__avg);
+            }
+            else{
+                review_item.push(data[i].review_item__name);
+                values
+            }
+
+        }
+        else*/
+            label_data.push(data[i].year);        
+    }
+    var colors = [];
+    for(var i  = 0; i < review_item.length; i++){
+        colors.push('rgba('+randomScalingFactor+','+randomScalingFactor+','+randomScalingFactor+',1)');
+    }
+    var datasetss = [];
+    for(var i = 0; i < review_item.length; i++){
+        datasetss.push({
+            label: 'Knowledge',
+            backgroundColor: 'rgba(173,220,202,0.2)',
+            borderColor: colors[i],
+            pointBackgroundColor: colors[i],
+            pointBorderColor: '#fff',
+            data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
+        });
+    }
+
     var lineChartData = {
-        labels: ['2001', '2003', '2005', '2007', '2009', '2011', '2013'],
-        datasets: [
-            {
-                label: 'Knowledge',
-                backgroundColor: 'rgba(173,220,202,0.2)',
-                borderColor: 'rgba(173,220,202,1)',
-                pointBackgroundColor: 'rgba(173,220,202,1)',
-                pointBorderColor: '#fff',
-                data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
-            },
-            {
-                label: 'Preparation',
-                backgroundColor: 'rgba(220,235,194,0.2)',
-                borderColor: 'rgba(220,235,194,1)',
-                pointBackgroundColor: 'rgba(220,235,194,1)',
-                pointBorderColor: '#fff',
-                data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
-            }
-            ,
-            {
-                label: 'Easy To Follow',
-                backgroundColor: 'rgba(254,210,183,0.2)',
-                borderColor: 'rgba(254,210,183,1)',
-                pointBackgroundColor: 'rgba(254,210,183,1)',
-                pointBorderColor: '#fff',
-                data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
-            }
-            ,
-            {
-                label: 'Helpful',
-                backgroundColor: 'rgba(239,98,112,0.2)',
-                borderColor: 'rgba(239,98,112,1)',
-                pointBackgroundColor: 'rgba(239,98,112,1)',
-                pointBorderColor: '#fff',
-                data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
-            }
-            ,
-            {
-                label: 'Respectful',
-                backgroundColor: 'rgba(244,234,200,0.2)',
-                borderColor: 'rgba(244,234,200,1)',
-                pointBackgroundColor: 'rgba(244,234,200,1)',
-                pointBorderColor: '#fff',
-                data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
-            }
-            ,
-            {
-                label: 'Encourage Preparation',
-                backgroundColor: 'rgba(33,52,67,0.2)',
-                borderColor: 'rgba(33,52,67,1)',
-                pointBackgroundColor: 'rgba(33,52,67,1)',
-                pointBorderColor: '#fff',
-                data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
-            }
-        ]
+        labels: label_data,
+        datasets: datasetss
     }
 
     var ctx = document.getElementById('canvas-1');
