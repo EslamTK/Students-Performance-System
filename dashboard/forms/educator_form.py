effe4d09d681b39346775285ae182c5fc81978ce
Eslam Tarek <eslamstm1@gmail.com>
2018-04-22 21:45:56 +0200

    Added administrator (educator accounts logos)

--- dashboard/forms/educator_form.py
+++ dashboard/forms/educator_form.py
@@ -23,5 +23,6 @@ class EducatorForm(forms.ModelForm):
                                                            can_delete=True)
 
         self.accounts_formset = self.EducatorAccountFormset(args[0],
+                                                            form_kwargs={'accounts': accounts},
                                                             queryset=educator_accounts,
                                                             initial=educator_not_accounts)
