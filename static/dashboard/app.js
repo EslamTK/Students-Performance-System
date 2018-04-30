95886bc51bc7e5f6037201e5a16f3b8013e852d2
Eslam Tarek <eslamstm1@gmail.com>
2018-04-30 22:04:39 +0200

    Fix the nav-links

--- static/dashboard/app.js
+++ static/dashboard/app.js
@@ -3,7 +3,7 @@
  */
 
 // Active ajax page loader
-$.ajaxLoad = true;
+$.ajaxLoad = false;
 
 //required when $.ajaxLoad = true
 $.defaultPage = '';
@@ -100,83 +100,6 @@ function loadCSS(cssFile, end, callback) {
 
 }
 
-/****
- * AJAX LOAD
- * Load pages asynchronously in ajax mode
- */
-
-if ($.ajaxLoad) {
-
-    var paceOptions = {
-        elements: false,
-        restartOnRequestAfter: false
-    };
-
-    var url = location.hash.replace(/^#/, '');
-
-    if (url != '') {
-        setUpUrl(url);
-    } else {
-        setUpUrl($.defaultPage);
-    }
-
-    $(document).on('click', '.nav a[href!="#"]', function (e) {
-        if ($(this).parent().parent().hasClass('nav-tabs') || $(this).parent().parent().hasClass('nav-pills')) {
-            e.preventDefault();
-        } else if ($(this).attr('target') == '_top') {
-            e.preventDefault();
-            var target = $(e.currentTarget);
-            window.location = (target.attr('href'));
-        } else if ($(this).attr('target') == '_blank') {
-            e.preventDefault();
-            var target = $(e.currentTarget);
-            window.open(target.attr('href'));
-        } else {
-            e.preventDefault();
-            var target = $(e.currentTarget);
-            setUpUrl(target.attr('href'));
-        }
-    });
-
-    $(document).on('click', 'a[href="#"]', function (e) {
-        e.preventDefault();
-    });
-}
-
-function setUpUrl(url) {
-
-    $('nav .nav li .nav-link').removeClass('active');
-    $('nav .nav li.nav-dropdown').removeClass('open');
-    $('nav .nav li:has(a[href="' + url.split('?')[0] + '"])').addClass('open');
-    $('nav .nav a[href="' + url.split('?')[0] + '"]').addClass('active');
-
-    loadPage(url);
-}
-
-function loadPage(url) {
-
-    $.ajax({
-        type: 'GET',
-        url: $.subPagesDirectory + url,
-        dataType: 'html',
-        cache: false,
-        async: false,
-        beforeSend: function () {
-            $.mainContent.css({opacity: 0});
-        },
-        success: function () {
-            Pace.restart();
-            $('html, body').animate({scrollTop: 0}, 0);
-            $.mainContent.load($.subPagesDirectory + url, null, function (responseText) {
-                window.location.hash = url;
-            }).delay(250).animate({opacity: 1}, 0);
-        },
-        error: function () {
-            window.location.href = $.page404;
-        }
-
-    });
-}
 
 /****
  * MAIN NAVIGATION
@@ -199,19 +122,6 @@ $(document).ready(function ($) {
             $(this).parents('ul').add(this).each(function () {
                 $(this).parent().addClass('open');
             });
-        }
-    });
-
-    // Dropdown Menu
-    $.navigation.on('click', 'a', function (e) {
-
-        if ($.ajaxLoad) {
-            e.preventDefault();
-        }
-
-        if ($(this).hasClass('nav-dropdown-toggle')) {
-            $(this).parent().toggleClass('open');
-            resizeBroadcast();
         }
     });
 
