e96e6cdf2d0685e0caf6a0e30ad445e627f12f4e
Mohamad <midomedo33@gmail.com>
2018-04-30 03:21:34 +0200

    ajax request done

--- static/dashboard/views/charts5.js
+++ static/dashboard/views/charts5.js
@@ -1,11 +1,38 @@
-$(function () {
+window.onload = default_;
+$('#year-selector').change(function () {	
+        selectedYear=$( '#year-selector option:selected' ).val();
+		//condition to disabe department_list till select year
+	    if(selectedYear === ''){
+			$("#department-selector").val($("#department-selector option:first").val());
+            $('#department-selector').attr('disabled', 'disabled');
+			default_();
+		}else{
+			$('#department-selector').attr('disabled', false);
+			//$('#yTitle').attr('disabled', 'disabled');
+			$('#dTitle').attr('disabled', 'disabled');
+		}	
+		console.log("Id: "+selectedYear);
+		$('#department-selector').change(function () {	
+			var selectedDep=$( '#department-selector option:selected' ).val();
+			// send selected year and dep to send_request function 
+			send_request(selectedYear,selectedDep);
+		});
+    });
+function send_request(selectedYear,selectedDep) {
     'use strict';
+	console.log("selectedYear: "+selectedYear);
+	console.log("selectedDep: "+selectedDep);
+    var depId = 'department_id='+selectedDep+'';
+    var yId = '&year='+selectedYear+'';
+    var bUrl = 'http://127.0.0.1:8000/dashboard/administrator_educators_rating?';
+    var aUrl = bUrl + depId + yId ;
+
     $.ajax({
-        url: 'http://127.0.0.1:8000/dashboard/administrator_educators_rating',
+        url: aUrl,
         dataType: "json",
         type: 'GET',
         contentType: 'application/json',
-        data: data,
+        data: JSON.stringify( { } ),
         success: function (data) {
             var label_data = [];   //year
             var review_item_label = []; // item_name
@@ -73,6 +100,74 @@ $(function () {
     var randomScalingFactor = function () {
         return Math.round(Math.random() * 255)
     };
+}
+
+function default_() {
+    'use strict';
+    var randomScalingFactor = function () {
+        return Math.round(Math.random() * 255)
+    };
 
     var data = educatorsRatingData;
-});
+
+    var label_data = [];
+    var review_item_label = [];
+    var review_item = {};
+
+    for (var i = 0; i < data.length; i++) {
+        if (!label_data.includes(data[i].year)) {
+            label_data.push(data[i].year);
+        }
+        if (review_item_label.includes(data[i].review_item__name)) {
+            review_item[data[i].review_item__name].push(data[i].rate__avg);
+            }
+        else {
+            review_item_label.push(data[i].review_item__name);
+            review_item[data[i].review_item__name] = new Array();
+            review_item[data[i].review_item__name].push(data[i].rate__avg);
+
+        }
+
+    }
+
+
+    var colors = [];
+    for (var i = 0; i < review_item_label.length; i++) {
+        colors.push('' + randomScalingFactor() + ',' + randomScalingFactor() + ',' + randomScalingFactor() + ',');
+    }
+    var datasetss = [];
+    for (var i = 0; i < review_item_label.length; i++) {
+        datasetss.push({
+            label: review_item_label[i],
+            backgroundColor: 'rgba(' + colors[i] + '0.2)',
+            borderColor: 'rgba(' + colors[i] + '1)',
+            pointBackgroundColor: 'rgba(' + colors[i] + '1)',
+            pointBorderColor: '#fff',
+            data: review_item[review_item_label[i]]
+        });
+
+    }
+
+    var lineChartData = {
+        labels: label_data,
+        datasets: datasetss
+    }
+
+    var ctx = document.getElementById('canvas-1');
+    var chart = new Chart(ctx, {
+        type: 'bar',
+        data: lineChartData,
+        options: {
+            responsive: true,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
+
+}
--- static/dashboard/views/charts6.js
+++ static/dashboard/views/charts6.js
@@ -1,69 +1,109 @@
-$(function () {
-    'use strict';
-    $.ajax({
-        url: 'http://127.0.0.1:8000/dashboard/educator_rating',
-        dataType: "json",
-        type: 'GET',
-        contentType: 'application/json',
-        data: data,
-        success: function (data) {
-            console.log(data.result);
-            // transform json array into normal array
-            var rate = data.result.map(function(result) {
-            return result.rate__avg;
-           }); 
-           var review = data.result.map(function(result) {
-            return result.review_item__name;
-           });      
-           console.log('rate_avg: ' + rate); 
-           console.log('review: ' + review);    
-           //////////
-           var barChartData = {
-                labels: review,
-                datasets: [
-                    {
-                        label: 'Encourage Preparation',
-                        backgroundColor: 'rgba(33,52,67,0.2)',
-                        borderColor: 'rgba(33,52,67,1)',
-                        pointBackgroundColor: 'rgba(33,52,67,1)',
-                        pointBorderColor: '#fff',
-                        data: rate
-                    }
-                ]
-            }
-
-            var ctx = document.getElementById('canvas-1');
-            var chart = new Chart(ctx, {
-                type: 'bar',
-                data: barChartData,
-                options: {
-                    scales: {
-                    yAxes: [{
-                        display: true,
-                        ticks: {
-                            beginAtZero: true,
-                                        stepSize: 0.3,
-                                        max: 5
-                               }
-                         }]
-                        },
-                    responsive: true
-                }
-            });
-           //////////
-        }
+var selectedYear;
+window.onload = default_;
+$('#year-selector').change(function () {	
+        selectedYear=$( '#year-selector option:selected' ).val();
+		//condition to disabe department_list till select year
+	    if(selectedYear === ''){
+			$("#department-selector").val($("#department-selector option:first").val());
+            $('#department-selector').attr('disabled', 'disabled');
+			default_();
+		}else{
+			$('#department-selector').attr('disabled', false);
+			//$('#yTitle').attr('disabled', 'disabled');
+			$('#dTitle').attr('disabled', 'disabled');
+		}	
+		console.log("Id: "+selectedYear);
+		$('#department-selector').change(function () {	
+			var selectedDep=$( '#department-selector option:selected' ).val();
+			// send selected year and dep to send_request function 
+			send_request(selectedYear,selectedDep);
+		});
     });
 
-    /*var randomScalingFactor = function () {
+var edu= $('#educator_id').val();
+console.log("sssss: "+edu);
+//method that get the selected year and dep and display new data on the chart
+function send_request(selectedYear,selectedDep) {
+	   'use strict';
+		console.log("selectedYear: "+selectedYear);
+		console.log("selectedDep: "+selectedDep);
+		var depId = 'department_id='+selectedDep+'';	
+		var cYear = '&year='+selectedYear+'';
+		var edId = '&educator_id='+edu+'';
+		var baseUrl = 'http://127.0.0.1:8000/dashboard/educator_rating?';
+		var aUrl;
+	    // check if educator_id exists or not
+		if(typeof edu === 'undefined'){
+			aUrl = baseUrl +  depId + cYear ;
+		}else{
+			aUrl = baseUrl +  depId + cYear + edId;
+		}
+	    console.log("URL: "+ aUrl);
+	
+		$.ajax({
+			url: aUrl,
+			dataType: "json",
+			type: 'GET',
+			contentType: 'application/json',
+			success: function (data) {
+				console.log(data.result);
+				var label_data = [];
+				var values = [];
+				for (var i = 0; i < data.result.length; i++) {
+					label_data.push(data.result[i].review_item__name);
+					values.push(data.result[i].rate__avg);
+				}
+				//console.log(label_data);
+			   var barChartData = {
+					labels: label_data,
+					datasets: [
+						{
+							label: 'Encourage Preparation',
+							backgroundColor: 'rgba(33,52,67,0.2)',
+							borderColor: 'rgba(33,52,67,1)',
+							pointBackgroundColor: 'rgba(33,52,67,1)',
+							pointBorderColor: '#fff',
+							data: values
+						}
+					]
+				}
+
+				var ctx = document.getElementById('canvas-1');
+				var chart = new Chart(ctx, {
+					type: 'bar',
+					data: barChartData,
+					options: {
+						/*scales: {
+						yAxes: [{
+							display: true,
+							ticks: {
+								beginAtZero: true,
+											stepSize: 0.3,
+											max: 5
+								   }
+							 }]
+							},*/
+						responsive: true
+					}
+				});
+			   //////////
+			}
+		});
+	}//end sendrequest function 
+
+
+    
+function default_() {
+    'use strict';
+	var randomScalingFactor = function () {
         return Math.round(Math.random() * 100)
     };
     var my_data = document.getElementById("myData").value;
-*/
+
     //formating data to valid json format
     var data = my_data.slice(10, my_data.length, my_data)
     data = data.replace('>', '');
     data = data.replace(/'/g, '"');
-   /*
     data = JSON.parse(data);
 
     var label_data = [];
@@ -71,5 +111,28 @@ $(function () {
     for (var i = 0; i < data.length; i++) {
         label_data.push(data[i].review_item__name);
         values.push(data[i].rate__avg);
-    }  */
-});
+    }
+
+    var barChartData = {
+        labels: label_data,
+        datasets: [
+            {
+                label: 'Encourage Preparation',
+                backgroundColor: 'rgba(33,52,67,0.2)',
+                borderColor: 'rgba(33,52,67,1)',
+                pointBackgroundColor: 'rgba(33,52,67,1)',
+                pointBorderColor: '#fff',
+                data: values
+            }
+        ]
+    }
+
+    var ctx = document.getElementById('canvas-1');
+    var chart = new Chart(ctx, {
+        type: 'bar',
+        data: barChartData,
+        options: {
+            responsive: true
+        }
+    });
+	}
