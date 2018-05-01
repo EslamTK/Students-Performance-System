318041c28fd0d3dd9837f4cdfdb915fa23e49368
Mohamad <midomedo33@gmail.com>
2018-05-01 15:42:47 +0200

    ajax request done

--- static/dashboard/views/charts3.js
+++ static/dashboard/views/charts3.js
@@ -1,11 +1,52 @@
-$(function () {
+//- term_id
+window.onload = default_;
+var selectedYear;
+$('#year-selector').change(function () {	
+        selectedYear=$( '#year-selector option:selected' ).val();
+		//condition to disabe department_list till select year
+	    if(selectedYear === ''){
+			$("#department-selector").val($("#department-selector option:first").val());
+            $('#department-selector').attr('disabled', 'disabled');
+			default_();
+		}else{
+			$('#department-selector').attr('disabled', false);
+			//$('#yTitle').attr('disabled', 'disabled');
+			$('#dTitle').attr('disabled', 'disabled');
+		}	
+		console.log("Id: "+selectedYear);
+		$('#department-selector').change(function () {	
+			var selectedDep=$( '#department-selector option:selected' ).val();
+			// send selected year and dep to send_request function 
+			send_request(selectedYear,selectedDep);
+		});
+    });
+
+//value onchange radio_button 
+$("input:radio[name=options]").change(function(){
+	var ter_id= $('input:radio[name=options]:checked').val();
+	console.log("here is : "+ter_id );
+});
+
+
+//default value of radio button
+var term_id=$("#rb").prop("checked", true).val();
+console.log("test_term: "+term_id);
+
+function send_request(selectedYear,selectedDep) {
     'use strict';
+    //var depId = $('#idname').val(); 
+    var depId = 'department_id='+selectedDep+'';
+    var yId = '&year='+selectedYear+'';
+    var tId = '&term_id='+term_id+'';
+    var bUrl = 'http://127.0.0.1:8000/dashboard/educator_courses_counts?';
+    var aUrl = bUrl +  depId + yId + tId;
+
     $.ajax({
-        url: 'http://127.0.0.1:8000/dashboard/educator_courses_counts',
+        url: aUrl,
         dataType: "json",
         type: 'GET',
         contentType: 'application/json',
-        data: data,
+        data: JSON.stringify( { } ),
         success: function (data) {
             console.log(data.result);
             // transform json array into normal array
@@ -29,12 +70,6 @@ $(function () {
               return ttl - fin_pass[idx];
             });
 
-            /*console.log('course_name: ' + course_name);
-            console.log('midterm_pass: ' + mt_pass);
-            console.log('final_pass: ' + fin_pass);
-            console.log('total_: ' + ttl);
-            console.log('mt_fail: ' + mt_fail);
-            console.log('final_fail: ' + fin_fail);*/
 /////////////
             var barChartData = {
             labels: course_name,
@@ -93,6 +128,13 @@ $(function () {
 ////////////
         }
         });
+    }
+
+
+
+
+function default_() {
+    'use strict';
 
     var randomScalingFactor = function () {
         return Math.round(Math.random() * 10)
@@ -104,9 +146,7 @@ $(function () {
     var data = my_data.slice(10, my_data.length, my_data)
     data = data.replace('>', '');
     data = data.replace(/'/g, '"');
-
-
-   /* data = JSON.parse(data);
+    data = JSON.parse(data);
 
     console.log(data);
     //collecting data
@@ -119,21 +159,67 @@ $(function () {
     for (var i = 0; i < data.length; i++) {
 
         if (data[i].midterm_pass == 0) {
-            //for data to be visual remove if valid data entered
-            //data[i].midterm_pass = randomScalingFactor();
-            //for data to be visual remove if valid data entered
-            //data[i].final_pass = randomScalingFactor();
-
+           
             label_data.push(data[i].course__name);
             midPass.push(data[i].midterm_pass);
             midFail.push(data[i].total - data[i].midterm_pass);
             finalPass.push(data[i].final_pass);
             finalFail.push(data[i].total - data[i].final_pass);
         }
-    }*/
+    }
 
 
+    var lineChartData = {
+        labels: label_data,
+        datasets: [
+            {
+                label: 'Midterm Pass',
+                backgroundColor: 'rgba(220,220,220,0.2)',
+                borderColor: 'rgba(220,220,220,1)',
+                pointBackgroundColor: 'rgba(220,220,220,1)',
+                pointBorderColor: '#fff',
+                data: midPass
+            },
+            {
+                label: 'Midterm Fail',
+                backgroundColor: 'rgba(151,187,205,0.2)',
+                borderColor: 'rgba(151,187,205,1)',
+                pointBackgroundColor: 'rgba(151,187,205,1)',
+                pointBorderColor: '#fff',
+                pointBorderColor: '#fff',
+
+                data: midFail
+            }
+            ,
+            {
+                label: 'Final Pass',
+                backgroundColor: 'rgba(234,209,204,0.2)',
+                borderColor: 'rgba(234,209,204,1)',
+                pointBackgroundColor: 'rgba(234,209,204,1)',
+
+                pointBorderColor: '#fff',
+                data: finalPass
+            }
+            ,
+            {
+                label: 'Final Fail',
+                backgroundColor: 'rgba(41,54,61,0.2)',
+                borderColor: 'rgba(41,54,61,1)',
+                pointBackgroundColor: 'rgba(41,54,61,1)',
+
+                pointBorderColor: '#fff',
+                data: finalFail
+            }
+        ]
+    }
 
 
-
-});
+    var ctx = document.getElementById('canvas-1');
+    var chart = new Chart(ctx, {
+        type: 'bar',
+        data: lineChartData,
+        options: {
+            responsive: true
+        }
+    });
+}
--- static/dashboard/views/charts4.js
+++ static/dashboard/views/charts4.js
@@ -1,28 +1,72 @@
-$(function () {
+//- term_id
+alert(57557);
+window.onload = default_;
+var selectedYear;
+$('#year-selector').change(function () {	
+        selectedYear=$( '#year-selector option:selected' ).val();
+		//condition to disabe department_list till select year
+	    if(selectedYear === ''){
+			$("#department-selector").val($("#department-selector option:first").val());
+            $('#department-selector').attr('disabled', 'disabled');
+			default_();
+		}else{
+			$('#department-selector').attr('disabled', false);
+			//$('#yTitle').attr('disabled', 'disabled');
+			$('#dTitle').attr('disabled', 'disabled');
+		}	
+		console.log("Id: "+selectedYear);
+		$('#department-selector').change(function () {	
+			var selectedDep=$( '#department-selector option:selected' ).val();
+			// send selected year and dep to send_request function 
+			send_request(selectedYear,selectedDep);
+		});
+    });
+
+//value onchange radio_button 
+$("input:radio[name=options]").change(function(){
+	var ter_id= $('input:radio[name=options]:checked').val();
+	console.log("here is : "+ter_id );
+});
+
+
+//default value of radio button
+var term_id=$("#rb").prop("checked", true).val();
+console.log("test_term: "+term_id);
+
+function send_request(selectedYear,selectedDep) {
     'use strict';
+    //var depId = $('#idname').val();
+	console.log("selectedYear: "+selectedYear);
+	console.log("selectedDep: "+selectedDep);
+    var depId = 'department_id='+selectedDep+'';
+    var yId = '&year='+selectedYear+'';
+    var tId = '&term_id='+term_id+'';
+    var bUrl = 'http://127.0.0.1:8000/dashboard/administrator_years_counts?';
+    var aUrl = bUrl +  depId + yId + tId;
     $.ajax({
-        url: 'http://127.0.0.1:8000/dashboard/administrator_years_counts',
+        url: aUrl,
         dataType: "json",
         type: 'GET',
         contentType: 'application/json',
-        data: data,
         success: function (data) {
             console.log(data);
-            var year = data.result.map(function(result) {
-            return result.year;
-           });
-            var success = data.result.map(function(result) {
-            return result.success;
-           });
-            var fail = data.result.map(function(result) {
-            return result.fail;
-           });
-            console.log('year: ' + year);
-            console.log('success: ' + success);
-            console.log('fail: ' + fail);
+            var label_data = [];
+            var success = [];
+            var fail = [];
+
+
+            for (var i = 0; i < data.result.length; i++) {
+
+                label_data.push(data.result[i].year);
+                success.push(data.result[i].success);
+                fail.push(data.result[i].fail);
+                console.log(data.result[i].year);
+                console.log(data.result[i].success);
+                console.log(data.result[i].fail);
+            }
             /////////
             var lineChartData = {
-                labels: year,
+                labels: label_data,
                 datasets: [
                     {
                         label: 'success',
@@ -46,7 +90,7 @@ $(function () {
 
             var ctx = document.getElementById('canvas-1');
             var chart = new Chart(ctx, {
-                type: 'line',
+                type: 'bar',
                 data: lineChartData,
                 options: {
                     scales: {
@@ -65,21 +109,23 @@ $(function () {
             ///////////
         }
     });
+}
+  
+function default_() {
+    'use strict';
 
-    /*var randomScalingFactor = function () {
+    var randomScalingFactor = function () {
         return Math.round(Math.random() * 100)
     };
 
-    
     //getting data from hidden input field
     var my_data = document.getElementById("myData").value;
-    console.log(my_data);*/
-
+    console.log(my_data);
     //formating data to valid json format
     var data = my_data.slice(10, my_data.length, my_data)
     data = data.replace('>', '');
     data = data.replace(/'/g, '"');
-    /*data = JSON.parse(data);
+    data = JSON.parse(data);
     console.log(data);
 
     var label_data = [];
@@ -93,6 +139,38 @@ $(function () {
         success.push(data[i].success);
         fail.push(data[i].fail);
     }
-*/
 
-});
+    var lineChartData = {
+        labels: label_data,
+        datasets: [
+            {
+                label: 'success',
+                backgroundColor: 'rgba(220,220,220,0.2)',
+                borderColor: 'rgba(220,220,220,1)',
+                pointBackgroundColor: 'rgba(220,220,220,1)',
+                pointBorderColor: '#fff',
+                data: success
+            },
+            {
+                label: 'Fail',
+                backgroundColor: 'rgba(151,187,205,0.2)',
+                borderColor: 'rgba(151,187,205,1)',
+                pointBackgroundColor: 'rgba(151,187,205,1)',
+                pointBorderColor: '#fff',
+                data: fail
+            }
+        ]
+    }
+
+
+    var ctx = document.getElementById('canvas-1');
+    var chart = new Chart(ctx, {
+        type: 'line',
+        data: lineChartData,
+        options: {
+            responsive: true
+        }
+    });
+
+
+}
