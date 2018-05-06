1c0fe8bbd6563f69534585ed305987dea329ce85
Eslam Tarek <eslamstm1@gmail.com>
2018-05-06 14:00:04 +0200

    Fix educator students chart

--- static/dashboard/views/charts3.js
+++ static/dashboard/views/charts3.js
@@ -8,7 +8,7 @@ $(function () {
     var my_data = document.getElementById("myVar").value;
 
     //formating data to valid json format
-    var data = my_data.slice(10, my_data.length, my_data)
+    var data = my_data.slice(10, my_data.length, my_data);
     data = data.replace('>', '');
     data = data.replace(/'/g, '"');
     data = JSON.parse(data);
@@ -22,73 +22,49 @@ $(function () {
     var finalFail = [];
 
     for (var i = 0; i < data.length; i++) {
-
-        if (data[i].midterm_pass == 0) {
-            //for data to be visual remove if valid data entered
-            //data[i].midterm_pass = randomScalingFactor();
-            //for data to be visual remove if valid data entered
-            //data[i].final_pass = randomScalingFactor();
-
             label_data.push(data[i].course__name);
             midPass.push(data[i].midterm_pass);
             midFail.push(data[i].total - data[i].midterm_pass);
             finalPass.push(data[i].final_pass);
             finalFail.push(data[i].total - data[i].final_pass);
-        }
     }
-
 
     var lineChartData = {
         labels: label_data,
         datasets: [
             {
                 label: 'Midterm Pass',
-                backgroundColor: 'rgba(220,220,220,0.2)',
-                borderColor: 'rgba(220,220,220,1)',
-                pointBackgroundColor: 'rgba(220,220,220,1)',
-                pointBorderColor: '#fff',
+                backgroundColor: 'rgba(144, 164, 174)',
                 data: midPass
             },
             {
                 label: 'Midterm Fail',
-                backgroundColor: 'rgba(151,187,205,0.2)',
-                borderColor: 'rgba(151,187,205,1)',
-                pointBackgroundColor: 'rgba(151,187,205,1)',
-                pointBorderColor: '#fff',
-                pointBorderColor: '#fff',
-
+                backgroundColor: 'rgba( 255, 236, 179 )',
                 data: midFail
             }
             ,
             {
                 label: 'Final Pass',
-                backgroundColor: 'rgba(234,209,204,0.2)',
-                borderColor: 'rgba(234,209,204,1)',
-                pointBackgroundColor: 'rgba(234,209,204,1)',
-
-                pointBorderColor: '#fff',
+                backgroundColor: 'rgba( 215, 204, 200 )',
                 data: finalPass
             }
             ,
             {
                 label: 'Final Fail',
-                backgroundColor: 'rgba(41,54,61,0.2)',
-                borderColor: 'rgba(41,54,61,1)',
-                pointBackgroundColor: 'rgba(41,54,61,1)',
-
-                pointBorderColor: '#fff',
+                backgroundColor: 'rgba( 200, 230, 201 )',
                 data: finalFail
             }
         ]
-    }
+    };
 
 
     var ctx = document.getElementById('canvas-1');
     var chart = new Chart(ctx, {
-        type: 'line',
+        type: 'bar',
         data: lineChartData,
         options: {
-            responsive: true
+            responsive: true,
+            beginAtZero: true
         }
     });
 
