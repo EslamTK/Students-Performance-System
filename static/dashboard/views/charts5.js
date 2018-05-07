3402fd38d13e83e07d6e4ffb3769fe5e490131fe
MohamadFeeshar <mohamed.feeshar@gmail.com>
2018-05-07 18:38:22 +0200

    charts -removed duplicate code-

--- static/dashboard/views/charts5.js
+++ static/dashboard/views/charts5.js
@@ -1,4 +1,6 @@
-window.onload = default_;
+var my_data = educatorsRatingData;
+
+window.onload = drawChart(my_data);
 var selectedDep="";
 var selectedYear="";
 $('#year-selector').change(function () {
@@ -30,84 +32,18 @@ function send_request() {
         type: 'GET',
         contentType: 'application/json',
         success: function (data) {
-            var label_data = []; //year
-            var review_item_label = []; // item_name
-            var review_item = {};
-
-            for (var i = 0; i < data.result.length; i++) {
-                if (!label_data.includes(data.result[i].year)) {
-                    label_data.push(data.result[i].year);
-                }
-                if (review_item_label.includes(data.result[i].review_item__name)) {
-                    review_item[data.result[i].review_item__name].push(data.result[i].rate__avg);
-                } else {
-                    review_item_label.push(data.result[i].review_item__name);
-                    review_item[data.result[i].review_item__name] = new Array();
-                    review_item[data.result[i].review_item__name].push(data.result[i].rate__avg);
-
-                }
-
-            }
-
-
-            var colors = [];
-            for (var i = 0; i < review_item_label.length; i++) {
-                colors.push('' + randomScalingFactor() + ',' + randomScalingFactor() + ',' + randomScalingFactor() + ',');
-            }
-
-            var datasetss = [];
-            for (var i = 0; i < review_item_label.length; i++) {
-                datasetss.push({
-                    label: review_item_label[i],
-                    backgroundColor: 'rgba(' + colors[i] + '0.2)',
-                    borderColor: 'rgba(' + colors[i] + '1)',
-                    pointBackgroundColor: 'rgba(' + colors[i] + '1)',
-                    pointBorderColor: '#fff',
-                    data: review_item[review_item_label[i]]
-                });
-
-            }
-
-            var barChartData = {
-                labels: label_data,
-                datasets: datasetss
-            }
-
-            var ctx = document.getElementById('canvas-1');
-            // erase old data from chart when update data 
-            if (window.chart !== undefined || window.chart !== null) {
-                window.chart.destroy();
-            }
-            window.chart = new Chart(ctx, {
-                type: 'bar',
-                data: barChartData,
-                options: {
-                    responsive: true,
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }]
-                    }
-                }
-            });
-
+            drawChart(data.result);
+            
         }
     });
 
-    var randomScalingFactor = function () {
-        return Math.round(Math.random() * 255)
-    };
 }
 
-function default_() {
+function drawChart(data) {
     'use strict';
     var randomScalingFactor = function () {
         return Math.round(Math.random() * 255)
     };
-
-    var data = educatorsRatingData;
 
     var label_data = [];
     var review_item_label = [];
